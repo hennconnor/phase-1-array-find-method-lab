5633b2d26a3f171065239f2a33f7301d215ec7aa
Connor Henn <connor.henn@yahoo.com>
2022-07-24 16:46:15 -0700

    commit tree 887d724611d20c6c271691785df6f844af3d73d8

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record){
+   let winningSeason = record.find(season => season.result === 'W')
+    if (winningSeason === undefined){
+        return undefined;
+    }     
+   
+   return winningSeason.year;
+    }
